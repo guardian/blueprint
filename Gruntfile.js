8fa093657709b3f8425d8e52d2fef4d3ec4e9fdf
Andrew Mason <andrew.mason@guardian.co.uk>
2013-11-20 15:05:23 +0000

    S3 deploy added

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,11 +11,35 @@ module.exports = function(grunt) {
           hostname: '*'
         }
       }
+    },
+
+    s3: {
+      options: {
+        debug: true,
+        bucket: 'gdn-stage',
+        access: 'public-read',
+        gzip: true,
+        gzipExclude: ['.jpg', '.jpeg', '.png', 'gif'],
+        headers: {
+          'Cache-Control': 'max-age=10, public'
+        }
+      },
+      src: {
+        upload: [
+          {
+            src: 'src/**/*',
+            rel: 'src',
+            dest: 'test/'
+          }
+        ]
+      }
     }
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-s3');
 
   grunt.registerTask('default', ['connect']);
+  grunt.registerTask('deploy', ['s3']);
 };
